59510592bfe8e55825d5a5f833a274ef7ac447c8
JhessReyes <jhessaureyes@gmail.com>
2023-03-04 21:46:05 -0600

    name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+let a = "Hola Mundo";
 
 function App() {
   return (
     <div className="App">
       <h1 className="btn btn-ghost">Tic Tac Toe</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">{a}</h1>
     </div>
   );
 }
