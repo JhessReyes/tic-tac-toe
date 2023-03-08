7f8eaf66b03993312752eb79864227320609c5fa
JhessReyes <jhessaureyes@gmail.com>
2023-03-07 22:00:54 -0600

    styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 function Square({ value, onClick }) {
   return (
-    <button
-      className="border border-4 px-2 py-2 border-primary  text-6xl"
-      onClick={onClick}>
+    <button className="square" onClick={onClick}>
       {value}
     </button>
   );
